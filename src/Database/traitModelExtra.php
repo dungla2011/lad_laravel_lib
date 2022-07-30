e599f7c07c5d51abce34cb8f037dca8b6121d0a5
dungla2011 <dungla2011@gmail.com>
2022-07-30 19:30:35 +0700

    init first lib laravel lad

---
+++ src/Database/traitModelExtra.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace LadLib\Laravel\Database;
+
+use Illuminate\Database\Eloquent\Model;
+
+trait traitModelExtra
+{
+    /**
+     * Get all fields of a table , and return data type of field
+     * @return array [fields => data type]
+     */
+    public function getTableColumns() {
+        if($this instanceof Model);
+
+        $allField = $this->getConnection()->getSchemaBuilder()->getColumnListing($this->getTable());
+        $ret = [];
+        foreach($allField AS $field_name){
+            $val = $this->getConnection()->getDoctrineColumn($this->getTable(), $field_name)->getType()->getName();
+            $ret [$field_name] = $val;
+            // echo "<br> $field_name | $val";
+        }
+        return $ret;
+    }
+}
+
